db9ea7b25ec87d2b3ce2423d5572244680e7c2f1
alexgantony <aleximu03@gmail.com>
2026-04-10 18:47:38 +0530

    feat: add register page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import MainLayout from './layouts/MainLayout';
 import Analytics from './pages/Analytics';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Register from './pages/Register';
 import Settings from './pages/Settings';
 
 function App() {
   return (
     <Routes>
       <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
       <Route element={<MainLayout />}>
         <Route path='/' element={<Home />} />
         <Route path='/analytics' element={<Analytics />} />
